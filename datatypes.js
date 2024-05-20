f36be829f96a5cbe76edab50523bb2ee28d2b7c8
24LP8019 24LP8019 <24LP8019@G301-CC0203-2.local>
2024-05-20 13:31:47 +0800

    java

---
+++ datatypes.js
@@ -0,0 +1,3 @@
+console.log = typeof "true"
+console.log = typeof "40"
+console.log= typeof "null"
---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("hello")
+
+var one = "one class"
+let two = " two class"
+const three = " three class"
+
+console.log("Comments can make code readable")
+//Comments can make code readable
+
+console.log("Comments can make code readable")
+/*
+Comments can make code readable
+Comments can make code readable
+*/
+
+
